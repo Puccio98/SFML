1da928de994ef155005f2d4d5944972064ca6f66
maltempoLuca <maltempoluca98@gmail.com>
2024-11-09 21:18:42 +0100

    Fix animation initialization

--- Entities/Components/Animation.cpp
+++ Entities/Components/Animation.cpp
@@ -15,7 +15,7 @@ void Animation::play(const float &dt, float mod_percent) {
     if (this->animationTimer >= this->animationDuration) {
         this->animationTimer = 0.f;
     }
-    
+
     // Handle single frame duration
     if (this->frameTimer >= this->frameDuration) {
         // Reset frameTimer
@@ -43,9 +43,13 @@ void Animation::reset() {
 
 Animation::Animation(sf::Sprite &sprite, sf::Texture &texture_sheet, float animation_duration,
                      int start_frame_x, int start_frame_y, int end_frames_x, int end_frames_y,
-                     int width, int height, bool canBeInterrupted) : sprite(sprite), textureSheet(texture_sheet), animationLength(end_frames_x - start_frame_x + 1),
-                                                                     frameDuration(animation_duration / animationLength), animationDuration(
-                animation_duration) {
+                     int width, int height, bool canBeInterrupted) : sprite(sprite), textureSheet(texture_sheet),
+                                                                     animationLength(end_frames_x - start_frame_x + 1),
+                                                                     frameDuration(
+                                                                             animation_duration /
+                                                                             (end_frames_x - start_frame_x + 1)),
+                                                                     animationDuration(
+                                                                             animation_duration) {
     this->width = width;
     this->height = height;
 
