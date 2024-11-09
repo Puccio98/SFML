4cd5daf5ca610a5355e82be0758c9b87705bfc5d
maltempoLuca <maltempoluca98@gmail.com>
2024-11-09 21:23:33 +0100

    Initialization body

--- Entities/Components/Animation.cpp
+++ Entities/Components/Animation.cpp
@@ -43,27 +43,24 @@ void Animation::reset() {
 
 Animation::Animation(sf::Sprite &sprite, sf::Texture &texture_sheet, float animation_duration,
                      int start_frame_x, int start_frame_y, int end_frames_x, int end_frames_y,
-                     int width, int height, bool canBeInterrupted) : sprite(sprite), textureSheet(texture_sheet),
-                                                                     animationLength(end_frames_x - start_frame_x + 1),
-                                                                     frameDuration(
-                                                                             animation_duration /
-                                                                             (end_frames_x - start_frame_x + 1)),
-                                                                     animationDuration(
-                                                                             animation_duration) {
-    this->width = width;
-    this->height = height;
-
-    this->frameTimer = 0.f;
-    this->animationTimer = 0.f;
-    this->startRect = sf::IntRect(start_frame_x * width, start_frame_y * height, width, height);
-    this->currentRect = this->startRect;
-    this->endRect = sf::IntRect(end_frames_x * width, end_frames_y * height, width, height);
-
+                     int width, int height, bool canBeInterrupted)
+        : sprite(sprite),
+          textureSheet(texture_sheet),
+          animationLength(end_frames_x - start_frame_x + 1),
+          frameDuration(animation_duration / (end_frames_x - start_frame_x + 1)),
+          animationDuration(animation_duration),
+          width(width),
+          height(height),
+          frameTimer(0.f),
+          animationTimer(0.f),
+          startRect(start_frame_x * width, start_frame_y * height, width, height),
+          currentRect(startRect),
+          endRect(end_frames_x * width, end_frames_y * height, width, height),
+          canBeInterrupted(canBeInterrupted) {
     this->sprite.setTexture(this->textureSheet, true);
     this->sprite.setTextureRect(this->startRect);
-
-    this->canBeInterrupted = canBeInterrupted;
 }
+
 
 bool Animation::isAnimationAboutToRestart(const float &dt, float mod_percent) const {
     float at = this->animationTimer + mod_percent * this->magicNumber * dt;
